8afd6b11cccadcdedcf01298b215a007cb72cc4c
Alex Sears <alexwsears@gmail.com>
2015-01-04 00:43:05 -0500

    Fixed referer matching by stripping off ending front slash

--- lib/models/access_token.rb
+++ lib/models/access_token.rb
@@ -2,6 +2,6 @@ class AccessToken < Sequel::Model
   many_to_one :users
 
   def valid_on_domain?(host)
-    domain == host.sub(/^https?:\/\//, '')
+    domain == host.sub(/^https?:\/\/([A-Za-z0-9.-]*)\/?$/, '\1')
   end
 end
